6d89a42aed4284a89b88b09f30d547a28b25d928
nujovich <nujovich@gmail.com>
2024-01-12 18:52:24 +0100

    feat: add routes, styles, tags

---
+++ src/scripts/nav.js
@@ -0,0 +1,4 @@
+// Description: Navigation bar script
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.toggle("expanded");
+});
